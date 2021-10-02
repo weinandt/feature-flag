0d74e75c1a975b3365f0e5ee643854382eff3d1a
Nick Weinandt <nick.weinandt@gmail.com>
2021-10-02 10:15:38 -0500

    Npm publishing working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Feature Flag
 Simple feature flag library for node js.
+
+Please don't use yet, still testing npm publishing.
+
+
+## TODO:
--- src/featureFlag.ts
+++ src/featureFlag.ts
@@ -1,5 +1,25 @@
+export type FlagValueLookupFunction = (flagNames: string[]) => Map<string, boolean>
+
+export interface FeatureFlagConfig {
+    flagValueLookUps: FlagValueLookupFunction[]
+    ignoreErrorsDuringLookup?: boolean // TODO: not yet implemented.
+}
+
 export class FeatureFlag {
-    public static getFeatureFlag(flagName: string): boolean {
+    private flagValueLookUps: FlagValueLookupFunction[]
+    private ignoreErrorsDuringLookup: boolean = true
+
+    constructor(config: FeatureFlagConfig){
+        this.flagValueLookUps = config.flagValueLookUps
+    }
+
+    public async initialize(): Promise<void> {
+
+    }
+
+    public isEnabled(flagName: string): boolean {
         return true
     }
+
+
 }
